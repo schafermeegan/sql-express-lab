3981bac1ca2689c8051a9668b5db1d5efe0295cd
unknown <megan.gallimore@gmail.com>
2019-05-29 16:32:58 -0400

    commit

---
+++ cartData.js
@@ -0,0 +1,34 @@
+let cartData = [
+    {
+        id: "0",
+        product: "Juicy Fruit Gum",
+        price: "$4.25",  
+        quantity: "5",
+    },
+    {
+        id: "1",
+        product: "Hershey Kisses",
+        price: "$2.05",
+        quantity: "2",
+    },
+    {
+        id: "2",
+        product: "Bulk Strawberries",
+        price: "$4.99",
+        quantity: "1",
+    },
+    {
+        id: "3",
+        product: "Cheerios",
+        price: "$3.75",
+        quantity: "1",
+    },
+    {
+        id: "4",
+        product: "Granola",
+        price: "$9.55",
+        quantity: "2",
+    }];
+
+
+    module.exports = cartData;
