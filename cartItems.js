fd913bea50ccee4ed489259dadcea600d6d85050
unknown <megan.gallimore@gmail.com>
2019-06-02 18:17:00 -0400

    switched the info in server to cartItem and vice versa

--- cartItems.js
+++ cartItems.js
@@ -1,84 +1,84 @@
-const express = require("express");
+const express = require('express');
+
 const cartItems = express.Router();
-//const cartData = require('./cartData');
 
-cartItems.get('/', (req, res) => {
-   res.send(cartData);
+const pg = require("pg");
+const pool = new pg.Pool({
+  user: "postgres",
+  password: "Isla5313",
+  host: "localhost",
+  port: 5432,
+  database: "ExpressShopDB",
+  ssl: false
 });
 
-    // accept POST request at URI: /cartItems
-    cartItems.post("/cartItems", (req, res) => {
-        console.log(req.body); // <-- this is the data that has been extracted from the reque
-        res.send("Adding an item..");
-        });
-
-
-    cartItems.put("/cartItems/:id", (req, res) => {
-        console.log(req.params.id); // <-- params for the URL
-        res.send("Got for a specific id: " + req.params.id);
-        });
-
-
-    // accept DELETE request at URI: /cartItems
-cartItems.delete("/cartItems/:id", (req, res) => {
-    console.log(req.params.id); // <-- params for the URL
-    res.send("Deleting an item..");
+cartItems.get("/", (req, res) => {
+    pool.query("SELECT * FROM shoppingcart;")
+    .then ( (results) => {
+        res.send(results.rows);
+    })
+    .catch ( (err) => {
+        res.send(err);
+    })
 });
 
+cartItems.post("/", (req, res) => {
+    let data = req.body;
+    pool.query("INSERT INTO shoppingcart (product, price, count) values($1::text, $2::float, $3::int, $4::text)",[data.product, data.price, data.count])
+    .then( () => {
+        res.status(201);
+        res.send(data.body);
+    })
+});
 
+cartItems.put("/:id", (req, res) => {
+    pool.query("UPDATE shoppingcart SET count=$1::int WHERE id=$2::int", [req.body.count, req.body.id])
+    .then( () => {
+        res.send("Updated");
+    })
+    .catch( (err) => {
+        res.send(err);
+    })
+});
+
+cartItems.delete("/:id", (req, res) => {
+    pool.query("DELETE FROM shoppingcart WHERE id=$1::int", [req.params.id])
+    .then( () => {
+        res.status(201);
+        res.send("Deleted");
+    })
+});
 
 module.exports = cartItems;
 
+// const express = require("express");
+// const cartItems = express.Router();
+// //const cartData = require('./cartData');
 
-
-
-
-// cartItems.post("/cartItems", (req, res) => {
-//     // console.log(req.body);
-//     res.send(cartData);
+// cartItems.get('/', (req, res) => {
+//    res.send(cartData);
 // });
-// // accept PUT request at URI: /cartItems
-// cartItems.put("/cartItems/:id", (req, res) => {
-    
-//     res.send("Updating a food for a specific id: " + req.params.id);
-// });
-// // accept DELETE request at URI: /cartItems
+
+//     // accept POST request at URI: /cartItems
+//     cartItems.post("/cartItems", (req, res) => {
+//         console.log(req.body); // <-- this is the data that has been extracted from the reque
+//         res.send("Adding an item..");
+//         });
+
+
+//     cartItems.put("/cartItems/:id", (req, res) => {
+//         console.log(req.params.id); // <-- params for the URL
+//         res.send("Got for a specific id: " + req.params.id);
+//         });
+
+
+//     // accept DELETE request at URI: /cartItems
 // cartItems.delete("/cartItems/:id", (req, res) => {
-   
-//     res.send("Deleting a food for a specific id: " + req.params.id);
+//     console.log(req.params.id); // <-- params for the URL
+//     res.send("Deleting an item..");
 // });
 
 
 
-    
-    // students.get("/cartItems/me", (req, res) => {
-    //     console.log(req.body);
-    //     res.send("Getting my foods...");
-    // });
-    //If a request was made to "/users/10", the code below would log the number 10 to the console.
-    // array.get("/cart-items/:id", (req, res) => {
-    //     console.log(req.params.id); // <-- params for the URL
-    //     res.send("Got for a specific id: " + req.params.id);
-    // });
-    // accept POST request at URI: /cart-items
+// module.exports = cartItems;
 
-    
-// app.use(express.static("./public"));
-
-
-// // //will refer to the 
-// // const cartItems = require("./cartItems");
-
-// // //Convert params and body into useable JSON
-
-
-// // app.use("/cartItems", cartItems);
-
-// // app.get('/api', (req, res) => {
-// //     res.send("Hello World");
-// // });
-
-// const port = 2000;
-
-// //run the server!
-// app.listen(port, () => console.log('listening on port: http://localhost:${port}'));
