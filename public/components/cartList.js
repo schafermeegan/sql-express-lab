06b0cec6015a338c8e722aae1ea9c18d0c0c4fd9
Meegan Schafer <45988365+schafermeegan@users.noreply.github.com>
2019-06-03 18:19:50 -0400

    Add files via upload

--- public/components/cartList.js
+++ public/components/cartList.js
@@ -1,18 +1,6 @@
 function CartList(CartService) {
   const ctrl = this;
 
-//     ctrl.getList = () => {
-//         CartService.getCartData()
-//         .then( (data) => {
-//             ctrl.cartItems = data;
-//         })
-//         .catch( (err) => {
-//             console.log(err);
-//         })
-//     };
-//     ctrl.getList();
-// }
-
 ctrl.addItem = (addedProduct, totalPrice, updateQuantity) => {
     let addedItem = {
         product: addedProduct,
@@ -40,8 +28,6 @@ ctrl.updateItem = (item) => {
     }
 }
 
-
-
 angular
 .module('CartApp')
 .component('cartList', {
@@ -49,26 +35,27 @@ angular
   template:
   `
 
+    <div class="container">
+        <form name="shoppingFrm" ng-submit="addItem()">
+            <input type="text" ng-model="addedProduct" name="newproduct" placeholder="Item" required /><br></br>
+            <input type="text" ng-model="totalPrice" name="newprice" placeholder="$0.00" required /><br></br>
+            <input type="text" ng-model="updatedQuantity" name="newquantity" placeholder="Quantity" required /><br></br>
+    
+            <div class="buttonClass">
+                <button class="addToCart" ng-click="addItem()">Submit</button> 
+            </div>
 
-    "addedProduct"
-    "totalPrice"
-    "updateQuantity"
-  <div>
-      <h2>Shopping Cart<h2>
-      </div>
-  <div>
-      <form ng-submit="addItem(item)">
-          Add Item
-          <input type="text" ng-model="form.text1">
-      </form>
-      <form ng-submit="removeItem(id)">
-          Remove Item
-          <input type="number" ng-model="form.number1">
-      </form>
-      <form ng-submit="updateItem(item)">
-          Update Item
-          <input type="text" ng-model="form.text2"
-      </form>
-  </div>
+        <ol>
+            <li ng-repeat="shopping in shoppingss | filter:test">
+                <label ng-click="cart.deleted = !cart.deleted">
+                    <span ng-class="{ deleted: cart.deleted }">{{ shopping.item }} </span>
+                    <button class="delete" ng-submit="removeItem($index)">X</button>
+                </label>
+            </li>
+        </ol>   
+        </form>
+    </div>
+
+
   `
 })
